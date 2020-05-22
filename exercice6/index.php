af4c56e9ab4021f5ce793240c735aafa1db2586c
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-22 13:31:54 +0200

    exercice 6 fini

---
+++ exercice6/index.php
@@ -0,0 +1,15 @@
+<?php 
+    $number=140; 
+    $number=$number + 30;
+    $number=$number / 2; 
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="utf-8"/>
+    <title>Exercice 6 PHP</title>
+</head>
+<body>
+    <p>Résultat: <?php echo $number; ?></p>
+</body>
+</html>
