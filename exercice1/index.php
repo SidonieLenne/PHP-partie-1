9ed4a6521a14125664966a361d4ecff22928b113
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-22 09:43:41 +0200

    exercice 1 fini

---
+++ exercice1/index.php
@@ -0,0 +1,12 @@
+<?php $object='Bonjour';
+$message=' le boulghour'?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Exercice 1 PHP</title>
+    </head>
+    <body>
+       <?php echo $object , $message?>
+    </body>
+</html>
