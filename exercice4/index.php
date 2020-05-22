4a24584e26042d1964cee2dca86198bbc2136a4e
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-22 11:45:56 +0200

    exercice 4 fini

---
+++ exercice4/index.php
@@ -0,0 +1,11 @@
+<?php $name='Sidonie';?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Exercice 4 PHP</title>
+    </head>
+    <body>
+       <p>Bonjour <?php echo $name; ?>, comment vas-tu?</p>
+    </body>
+</html>
