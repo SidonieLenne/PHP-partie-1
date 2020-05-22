25a6190fa8b071cb745d477cf4ef2cbeb42d27da
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-22 11:37:29 +0200

    exercice 3 fini

---
+++ exercice3/index.php
@@ -0,0 +1,12 @@
+<?php $km=1;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Exercice 3 PHP</title>
+    </head>
+    <body>
+       <?php echo $km; $km=3; ?>, <?php echo $km; $km=125; ?>, <?php echo $km; ?>
+    </body>
+</html>
