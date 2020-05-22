2399952eeb2b3671997d2ba97cabb20bb1858bab
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-22 10:05:50 +0200

    exercice 2 fini

---
+++ exercice2/index.php
@@ -0,0 +1,17 @@
+<?php $lastname='Lenne ';
+    $firstname='Sidonie';
+    $age=21
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Exercice 2 PHP</title>
+    </head>
+    <body>
+       <?php 
+       //echo 'Je m\'appelle ' .$lastname , $firstname. ' et j\'ai '.$age.' ans'  Version simple quotes et points
+       echo "Je m'appelle $lastname $firstname et j'ai $age ans"  //Version double quotes
+       ?>
+    </body>
+</html>
